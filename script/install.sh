f24b4ad78d5839e0c4c139157d9cb6fe8327f397
Samip Timalsena <samip425@gmail.com>
2021-04-28 20:33:21 +0545

    update: install.sh

--- script/install.sh
+++ script/install.sh
@@ -6,6 +6,7 @@ conda activate image_search
 #installing pytorch and CLIP
 conda install --yes -c pytorch pytorch=1.7.1 torchvision cudatoolkit=11.0
 pip3 install ftfy regex tqdm
+pip3 install opencv-python
 pip3 install git+https://github.com/openai/CLIP.git
 
 #downloading one of the available clip model
